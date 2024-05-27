59a9ecb165e73fb9179aac40cc16133ab2e935c2
miss55 <159412650+tpraka300trac@users.noreply.github.com>
2024-05-27 15:58:07 +0200

    Update deploy.ts

--- src/contracts/deploy.ts
+++ src/contracts/deploy.ts
@@ -7,38 +7,18 @@ import {
 } from '@abacus-network/sdk';
 
 import { AbcERC721Config } from '../consts/networksConfig';
+import { AbcERC721Contracts, AbcERC721Factories } from './contracts';
 
-import {
-  AbcERC721Contracts,
-  AbcERC721Factories,
-  abcERC721Factories,
-} from './contracts';
-
-export class AbcERC721Deployer<
-  Chain extends ChainName,
-> extends AbacusRouterDeployer<
-  Chain,
-  AbcERC721Contracts,
-  AbcERC721Config,
-  AbcERC721Factories
-> {
-  constructor(
-    multiProvider: MultiProvider<Chain>,
-    configMap: ChainMap<Chain, AbcERC721Config>,
-    protected core: AbacusCore<Chain>,
-  ) {
+export class AbcERC721Deployer<Chain extends ChainName> extends AbacusRouterDeployer<Chain, AbcERC721Contracts, AbcERC721Config, AbcERC721Factories> {
+  constructor(multiProvider: MultiProvider<Chain>, configMap: ChainMap<Chain, AbcERC721Config>, protected core: AbacusCore<Chain>) {
     super(multiProvider, configMap, abcERC721Factories, {});
   }
 
   // Custom contract deployment logic can go here
   // If no custom logic is needed, call deployContract for the router
   async deployContracts(chain: Chain, config: AbcERC721Config) {
-    const router = await this.deployContract(chain, 'router', [
-      config.abacusConnectionManager,
-      config.interchainGasPaymaster,
-    ]);
-    return {
-      router,
-    };
+    const { abacusConnectionManager, interchainGasPaymaster } = config;
+    const router = await this.deployContract(chain, 'router', [abacusConnectionManager, interchainGasPaymaster]);
+    return { router };
   }
 }
